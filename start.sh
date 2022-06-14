12d751adf36c904cd6e98ec565e96ee2313376e7
Sachin S <86321236+sachin9742s@users.noreply.github.com>
2022-06-14 19:09:02 +0530

    Add files via upload

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/sachin9742s/Rocky_autofilter_Robot.git /Rocky_autofilter_Robot
+  git clone https://github.com/PR0FESS0R-99/LuciferMoringstar-Robot.git /LuciferMoringstar-Robot
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /Rocky_autofilter_Robot
+  git clone $UPSTREAM_REPO /LuciferMoringstar-Robot
 fi
-cd /Rocky_autofilter_Robot
+cd /LuciferMoringstar-Robot
 pip3 install -U -r requirements.txt
-echo "Starting Rocky_autofilter_Robot RoBot...."
+echo "Starting LuciferMoringstar RoBot...."
 python3 main.py
