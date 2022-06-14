f8916f07774c3e3446d667bb482d60f100c8835c
Sachin S <86321236+sachin9742s@users.noreply.github.com>
2022-06-14 19:17:08 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/PR0FESS0R-99/LuciferMoringstar-Robot.git /LuciferMoringstar-Robot
+  git clone https://github.com/sachin9742s/Rocky_autofilter_Robot.git /LuciferMoringstar-Robot
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /LuciferMoringstar-Robot
+  git clone $UPSTREAM_REPO /Rocky_autofilter_Robot
 fi
 cd /LuciferMoringstar-Robot
 pip3 install -U -r requirements.txt
-echo "Starting LuciferMoringstar RoBot...."
+echo "Starting Rocky autofilter Robot...."
 python3 main.py
